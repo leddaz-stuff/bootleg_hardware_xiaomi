45b14489aaab7d651a2fa669cdec60d5bf7697f0
Leonardo Ledda <leonardoledda@gmail.com>
2023-04-20 00:59:06 +0200

    aidl: Drop powershare
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- aidl/powershare/product.mk
+++
@@ -1,7 +0,0 @@
-# Powershare
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/hardware/xiaomi/aidl/powershare/framework_compatibility_matrix.xml
-
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare-service.xiaomi
-
-BOARD_VENDOR_SEPOLICY_DIRS += vendor/hardware/xiaomi/sepolicy/powershare
